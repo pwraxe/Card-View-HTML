1770054da8adbfb1a3241de892ebfed57bcf103a
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2019-06-28 13:48:02 +0530

    Create cardView.php

---
+++ cardView.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Home Page</title>
+		<style type="text/css">
+			.card{
+				box-shadow: 0 4px 8px 0 rgba(0,0,0,0.6);
+				transition: 0.4s;
+				width: 350px;
+				margin-top: 100px;
+			}
+			img{
+				width: 350px;
+			}
+
+		</style>
+	</head>
+	<body>
+		<center>
+			<div class="card">
+			  <img src="user3.jpg">
+			  <h1>Akshay Pawar</h1>
+		  	<p>pawarakshay13@gmail.com</p>
+		  	<p>9657513437</p>		
+		</div>
+		</center>
+	</body>
+</html>
